dcda7df5a19074dc3ef9459f088480ae3f3beee1
jeremiasz-goti <jeremiasz.goti@gmail.com>
2020-08-02 22:51:52 +0200

    improved models and basic task runner

--- dajgrypsa.py
+++ dajgrypsa.py
@@ -3,11 +3,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 from flask_sqlalchemy import SQLAlchemy
+from apscheduler.schedulers.background import BackgroundScheduler
 
+from datetime import datetime, timedelta
 import secrets
 import string
-
-alphabet = string.ascii_letters + string.digits
 
 # --- APP CONFIG
 app = Flask(__name__)
@@ -15,14 +15,25 @@ db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'dev'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///grypsy.db'
 
+# --- TASK RUNNER
+def check_database():
+    print("Checking database " + str(datetime.utcnow()))
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(check_database,'interval',minutes=1)
+sched.start()
+
+
 # --- DATABASE MODELS
 class Gryps(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     gryps_id = db.Column(db.String(10), unique=True)
     gryps_content = db.Column(db.String(300), unique=False, nullable=True)
+    gryps_creation = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    gryps_destroy = db.Column(db.DateTime, nullable=False, default=(datetime.utcnow() + timedelta(hours=24)))
 
     def __repr__(self):
-        return f"Gryps('{self.id}', '{self.gryps_id}', '{self.gryps_content}')"
+        return f"Gryps('{self.id}', '{self.gryps_id}', '{self.gryps_content}, '{self.gryps_creation}', '{self.gryps_destroy}')"
 
 # -- FORMS --
 class GrypsAdd(FlaskForm):
@@ -37,6 +48,7 @@ class GrypsDestroy(FlaskForm):
 def home():
     form = GrypsAdd()
     if form.validate_on_submit():
+        alphabet = string.ascii_letters + string.digits
         gryps_id = ''.join(secrets.choice(alphabet) for i in range(10))
         gryps = Gryps(gryps_id=gryps_id, gryps_content=form.gryps.data)
         db.session.add(gryps)
